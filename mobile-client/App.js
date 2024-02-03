328fd980a15e33eeb7864307223a58c1fb175b83
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2024-02-03 18:13:16 -0500

    test serialport lib

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  DeviceEventEmitter,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
+import { RNSerialport, definitions, actions } from "react-native-serialport";
 
 export default function App() {
   const [location, setLocation] = useState(null);
+  const [attached, setAttached] = useState(false);
+  const [output, setOutput] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -13,11 +22,41 @@ export default function App() {
         setLocation(location);
       });
     })();
+
+    DeviceEventEmitter.addListener(actions.ON_DEVICE_ATTACHED, () => {
+      setAttached(true);
+    });
+
+    DeviceEventEmitter.addListener(actions.ON_DEVICE_DETACHED, () => {
+      setAttached(false);
+    });
+
+    DeviceEventEmitter.addListener(actions.ON_ERROR, (error) => {
+      console.log("Error", error);
+    });
+
+    DeviceEventEmitter.addListener(actions.ON_READ_DATA, (data) => {
+      setOutput(
+        (prevOutput) => prevOutput + RNSerialport.hexToUtf16(data.payload)
+      );
+    });
+
+    RNSerialport.setReturnedDataType(definitions.RETURNED_DATA_TYPES.HEXSTRING);
+    RNSerialport.setAutoConnect(true);
+    RNSerialport.setAutoConnectBaudRate(9600);
+    RNSerialport.startUsbService();
+
+    return () => {
+      DeviceEventEmitter.removeAllListeners();
+      RNSerialport.disconnect();
+      RNSerialport.stopUsbService();
+    };
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Compass heading: {Math.round(location?.trueHeading)}°</Text>
+      <Text>Connection status: {attached ? "Connected" : "Disconnected"}</Text>
     </View>
   );
 }
