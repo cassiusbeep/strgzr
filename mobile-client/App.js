f3b9a7d3ca6a9c66ab8e402d95ba6211809c9acb
Miles Acquaviva <milesacq@icloud.com>
2024-02-03 15:26:22 -0500

    compass

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -1,27 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import CompassHeading from 'react-native-compass-heading';
 import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
   const [heading, setHeading] = useState("")
 
   useEffect(() => {
-    const degree_update_rate = 3;
-
-    CompassHeading.start(degree_update_rate, ({heading, accuracy}) => {
-      setHeading(heading)
-    });
-
-    // return () => {
-    //   CompassHeading.stop();
-    // };
-  }, []);
+    Location.getHeadingAsync().then((e) => setHeading(e.magHeading))
+  }, [heading]);
   return (
     <View style={styles.container}>
-      {/* <Button>
-        Align
-      </Button> */}
       <Text>Compass heading: {heading}</Text> 
     </View>
   );
