96dfd55d0c87e1ef029a26d81e9184be4d651d33
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2024-02-03 17:46:12 -0500

    test usbserial lib

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -1,18 +1,38 @@
 import { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Platform } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
+import { UsbSerial } from "react-native-usbserial";
 
 export default function App() {
   const [location, setLocation] = useState(null);
+  
+  let usbs;
 
   useEffect(() => {
-    (async () => {
+    const watchLocation = async () => {
       await Location.requestForegroundPermissionsAsync();
       Location.watchHeadingAsync((location) => {
         setLocation(location);
       });
-    })();
+    };
+
+    watchLocation();
+
+    if (Platform.OS === "android") {
+      usbs = new UsbSerial();
+      const getDevice = async () => {
+        const deviceList = await usbs.getDeviceListAsync();
+        const firstDevice = deviceList[0];
+        console.log(firstDevice);
+
+        if (firstDevice) {
+          const usbSerialDevice = await usbs.openDeviceAsync(firstDevice);
+          console.log(usbSerialDevice);
+        }
+      };
+      getDevice();
+    }
   }, []);
 
   return (
