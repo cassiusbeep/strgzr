2f3b3cfd8ac8b036c083b75f27dbcbed9be6cd81
Miles Acquaviva <milesacq@icloud.com>
2024-02-04 11:30:36 -0500

    New (#5)
    * things r broken
    
    * weird gyp ERR!
    
    * usbs?
    
    * add lock files
    
    * workin on server stuff
    
    * axios request bug
    
    * fixed axios thing :) now just need to fix the planet name passing error
    
    * axios req not working due to undefined planet name in App.js moveArm()
    
    * cleanup files
    
    * starting on server comms
    
    * styling and cleaning up arduino file
    
    * added bytewise reading to arduino
    
    * arduino retry
    
    * fix
    
    * compiles this time
    
    * trying new ard method
    
    * fixed planet name issue
    
    * last min ui stuff
    
    * added nice background
    
    ---------
    
    Co-authored-by: cassiusbeep <cassius_hall@brown.edu>
    Co-authored-by: aaron-ang <67321817+aaron-ang@users.noreply.github.com>

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -1,12 +1,23 @@
-import * as React from 'react';
+import * as React from "react";
 import { useEffect, useState } from "react";
-import { Pressable, Button, StyleSheet, Text, View, Platform, FlatList, SafeAreaView, Modal } from "react-native";
+import { ImageBackground } from "react-native";
+import {
+  Pressable,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  FlatList,
+  SafeAreaView,
+  Modal,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
 import { UsbSerial } from "react-native-usbserial";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import axios from 'axios';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import axios from "axios";
 
 export default function App() {
   const [usbConnected, setUsbConnected] = useState(false);
@@ -21,32 +32,33 @@ export default function App() {
   const [invisErrorOpen, setInvisErrorOpen] = useState(false);
 
   const getDirections = (long, lat, planet) => {
-    axios.get('http://server.milesacq.com:7892/calculate_distance',
-      {
+    axios
+      .get("http://server.milesacq.com:7892/calculate_distance", {
         params: {
           longitude: long,
           latitude: lat,
           planet: planet,
-          heading: location
+          heading: location,
+        },
+      })
+      .then(
+        (response) => {
+          setAzimuth(response.data.Azimuth);
+          setAltitude(response.data.Altitude);
+        },
+        (error) => {
+          console.log(error);
         }
-      }
-    ).then((response) => {
-      setAzimuth(response.data.Azimuth);
-      setAltitude(response.data.Altitude);
-    }, (error) => {
-      console.log(error);
-    })
-  }
+      );
+  };
 
   const sendDirections = (dirxns) => {
-    axios.post('http://server.milesacq.com:7892/move_servos',
-      {
-        params: {
-          dirString: dirxns
-        }
-      }
-    )
-  }
+    axios.post("http://server.milesacq.com:7892/move_servos", {
+      params: {
+        dirString: dirxns,
+      },
+    });
+  };
 
   // const getPlanets = (long, lat, direction) => {
   //   planetRouter.get(
@@ -75,8 +87,8 @@ export default function App() {
       await Location.watchPositionAsync({}, (location) => {
         setLong(location.coords.longitude);
         setLat(location.coords.latitude);
-      })
-    }
+      });
+    };
 
     watchLocation();
     watchPosition();
@@ -101,18 +113,24 @@ export default function App() {
   function HomeScreen({ navigation }) {
     useEffect(() => {
       if (usbConnected && usbs) {
-        navigation.navigate('Dock')
+        navigation.navigate("Dock");
       }
-    }, [usbConnected])
+    }, [usbConnected]);
 
     return (
       <View style={styles.container}>
-        <Text>Compass heading: {Math.round(location?.trueHeading)}°</Text>
-        <Text>Place phone in telescope dock!</Text>
-        <Pressable onPress={() => {
-          navigation.navigate('Dock')
-        }}
-        style={styles.button}><Text style={styles.buttonText}>Search the Stars</Text></Pressable>
+        <Text style={{ color: "white" }}>
+          Compass heading: {Math.round(location?.trueHeading)}°
+        </Text>
+        <Text style={{ color: "white" }}>Place phone in telescope dock!</Text>
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Dock");
+          }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Search the Stars</Text>
+        </Pressable>
       </View>
     );
   }
@@ -124,83 +142,113 @@ export default function App() {
     // if (altitude < 0 || altitude > 90) {
     //   console.log(planetName + " not visible"); // dont move the arm!
     // } else {
-      // TODO: check if this is appropriate calibration to turn the middle wheel
-      const xSteps = Math.round(((azimuth - location?.trueHeading) / 5.625) * 0.5);
-      const ySteps = Math.round(altitude / 5.625);
-      const dirString = xSteps + "," + ySteps + "!";
-      // send arduino direction string to the server to be passed into usb serial port
-      sendDirections(dirString);
-      setLastDirs(dirString);
-      navigation.push('Telescope');
+    // TODO: check if this is appropriate calibration to turn the middle wheel
+    const xSteps = Math.round(
+      ((azimuth - location?.trueHeading) / 5.625) * 0.5
+    );
+    const ySteps = Math.round(altitude / 5.625);
+    const dirString = xSteps + "," + ySteps + "!";
+    // send arduino direction string to the server to be passed into usb serial port
+    sendDirections(dirString);
+    setLastDirs(dirString);
+    navigation.push("Telescope");
     // }
   }
 
   BODIES = [
     {
-      id: '10',
-      title: 'sun',
+      id: "10",
+      title: "Sun",
     },
     {
-      id: '199',
-      title: 'mercury',
+      id: "199",
+      title: "Mercury",
     },
     {
-      id: '399',
-      title: 'earth',
+      id: "399",
+      title: "Earth",
     },
     {
-      id: '299',
-      title: 'venus',
+      id: "299",
+      title: "Venus",
     },
     {
-      id: '301',
-      title: 'moon',
+      id: "301",
+      title: "Moon",
     },
     {
-      id: '499',
-      title: 'mars',
+      id: "499",
+      title: "Mars",
     },
-  ]
+  ];
 
-  const Item = ({navigation, title}) => (
+  const Item = ({ navigation, title }) => (
     <View style={styles.item}>
-      <Pressable onPress={() => moveArm(navigation, title)} style={styles.button}><Text style={styles.buttonText}>{title}</Text></Pressable>
+      <Pressable
+        onPress={() => moveArm(navigation, title)}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>{title}</Text>
+      </Pressable>
     </View>
   );
 
   function DockScreen({ navigation }) {
+    // Sample image URLs for each column
+    const columnImages = [
+      "https://science.nasa.gov/wp-content/uploads/2023/05/pia03149-copy.jpg",
+      "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL21lcmN1cnktdXBkYXRlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fX19",
+      "https://climate.nasa.gov/system/internal_resources/details/original/309_ImageWall5_768px-60.jpg",
+      "https://cdn.mos.cms.futurecdn.net/RifjtkFLBEFgzkZqWEh69P-1200-80.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/1200px-FullMoon2010.jpg",
+      "https://science.nasa.gov/wp-content/uploads/2016/05/1-mars-nasa-gov-jpg.webp?w=4096&format=png&crop=1",
+    ];
+
+    const stylesa = StyleSheet.create({
+      container: {
+        flex: 1,
+        flexDirection: "row",
+      },
+      column: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "black",
+      },
+      image: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        resizeMode: "cover",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      text: {
+        fontSize: 18,
+        color: "white", // Adjust text color for better visibility
+      },
+    });
+
     return (
-      <View style={styles.container}>
-        <Text style={{marginTop: '160px'}}>Compass heading: {Math.round(location?.trueHeading)}°</Text>
-
-        <Pressable 
-          onPress={() => { navigation.goBack() }} 
-          title={"back"} 
-          style={styles.button}>
-          <Text style={styles.buttonText}>back</Text>
-        </Pressable>
-
-        {/* <Modal
-        animationType='slide'
-        transparent={false}
-        visible={invisErrorOpen}
-        onRequestClose={() => {
-          setInvisErrorOpen(false);
-        }}> */}
-          {/* <View style={styles.modalView}>
-            <Text style={styles.modalText}>Planet is not visible!</Text>
-            <Pressable onPress={() => setInvisErrorOpen(!invisErrorOpen)} style={styles.button}>
-              <Text style={styles.buttonText}>close</Text>
-            </Pressable>
-          </View> */}
-        {/* </Modal> */}
-        <SafeAreaView style={styles.container}>
-          <FlatList
-            data={BODIES}
-            renderItem={({item}) => <Item navigation={navigation} title={item.title}/>}
-            keyExtractor={item => item.id}
-            />
-        </SafeAreaView>
+      <View style={stylesa.container}>
+        {columnImages.map((image, index) => (
+          <Pressable
+          key={index}
+            onPress={() => {
+              moveArm(navigation, BODIES[index].title)
+            }} 
+            style={{alignSelf: 'stretch', width: '16.66666%'}}
+          >
+            <ImageBackground
+              
+              source={{ uri: image }}
+              style={stylesa.column}
+            >
+              <Text style={stylesa.text}>{BODIES[index].title}</Text>
+            </ImageBackground>
+          </Pressable>
+        ))}
       </View>
     );
   }
@@ -209,28 +257,38 @@ export default function App() {
     // find the opposite of the directions it took to orient here for the exit
     lastXStep = lastDirs.split(",")[0];
     lastYStep = lastDirs.split(",")[1];
-    const resetDirs = (-1 * lastXStep) + "," + (-1 * lastYStep);
+    const resetDirs = -1 * lastXStep + "," + -1 * lastYStep;
     return (
       <View style={styles.container}>
-        <Text>Compass heading: {Math.round(location?.trueHeading)}°</Text>
-        <Text>NOW VIEWING: {typeof currentPlanet == String ? currentPlanet : ""}</Text>
-        <Pressable onPress={() => {
-          // sendDirections(resetDirs);
-          setCurrentPlanet(null);
-          navigation.goBack();}}
-          style={styles.button}><Text style={styles.buttonText}>back</Text></Pressable>
+        <Text style={{ color: "white" }}>
+          Compass heading: {Math.round(location?.trueHeading)}°
+        </Text>
+        <Text style={{ color: "white" }}>
+          NOW VIEWING: {typeof currentPlanet == String ? currentPlanet : ""}
+        </Text>
+        <Pressable
+          onPress={() => {
+            // sendDirections(resetDirs);
+            setCurrentPlanet(null);
+            navigation.goBack();
+          }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>back</Text>
+        </Pressable>
       </View>
     );
   }
 
   return (
     <NavigationContainer>
-      <Stack.Navigator 
+      <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{
           headerTransparent: true,
-          headerBlurEffect: 'systemChromeMaterialLight'
-        }}>
+          headerBlurEffect: "systemChromeMaterialLight",
+        }}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -254,35 +312,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: 'flex',
+    display: "flex",
     backgroundColor: "#592A88",
     alignItems: "center",
     justifyContent: "center",
-    width: '100%',
-    color: '#fff',
-    marginTop: '125px',
+    width: "100%",
+    color: "#fff",
+    marginTop: "125px",
   },
   item: {
-    backgroundColor: '#592A88',
+    backgroundColor: "#592A88",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-    color: '#fff',
+    color: "#fff",
   },
   title: {
     fontSize: 32,
-    color: '#fff',
+    color: "#fff",
   },
   buttonText: {
     fontSize: 20,
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center",
   },
   button: {
     borderRadius: 20,
     padding: 10,
     elevation: 2,
-    backgroundColor: '#461C71',
-    color: '#fff',
+    backgroundColor: "#461C71",
+    color: "#fff",
   },
 });
--- star-server/main.py
+++ star-server/main.py
@@ -2,16 +2,56 @@ from skyfield.api import load
 from skyfield.api import N, W, wgs84
 from astropy import units as u
 import sys
+from flask_cors import CORS, cross_origin
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/calculate_distance', methods=['GET'])
+location_data = None
+servo_data = None
+
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+
+
+@app.route("/get_data", methods=["GET"])
+@cross_origin()
+def get_data():
+    global location_data
+    if location_data is None:
+        return "No data to send!", 500
+    return {
+        "longitude": location_data.longitude,
+        "latitude": location_data.latitude,
+        "heading": location_data.heading,
+    }
+
+app.route("/move_servos", methods=["POST"])
+@cross_origin()
+def move_servos():
+    global servo_data
+    servo_data = request.args.get("dirString")
+
+app.route("/get_servo_data", methods=["GET"])
+@cross_origin()
+def get_servo_data():
+    global servo_data
+    return servo_data
+
+@app.route("/calculate_distance", methods=["GET"])
+@cross_origin()
 def calculate_distance():
-    longitude = float(request.args.get('longitude'))
-    latitude = float(request.args.get('latitude'))
-    planet_name = request.args.get('planet')
+    longitude = float(request.args.get("longitude"))
+    latitude = float(request.args.get("latitude"))
+    planet_name = request.args.get("planet")
+    heading = float(request.args.get("heading[magHeading]"))
+    global location_data
+    location_data = {
+        "longitude": longitude,
+        "latitude": latitude,
+        "heading": heading,
+    }
 
     ts = load.timescale()
     t = ts.now()
@@ -35,11 +75,13 @@ def calculate_distance():
 
     return result
 
-@app.route('/closest_planet', methods=['GET'])
+
+@app.route("/closest_planet", methods=["GET"])
+@cross_origin()
 def closest_planet():
-    latitude = float(request.args.get('latitude'))
-    longitude = float(request.args.get('longitude'))
-    direction = float(request.args.get('direction'))
+    latitude = float(request.args.get("latitude"))
+    longitude = float(request.args.get("longitude"))
+    direction = float(request.args.get("direction"))
     planets = load("de421.bsp")
     planets_visible = [
         planets["venus"],
@@ -62,15 +104,14 @@ def closest_planet():
         alt, az, d = astrometric.apparent().altaz()
         print(direction)
         print(az.degrees)
-        if (az.degrees - direction < distance):
+        if az.degrees - direction < distance:
             closest_name = str(planet)
             closest_name = closest_name.split(" ")
             closest_name = closest_name[len(closest_name) - 1]
 
-    result = {"name":closest_name}
-
+    result = {"name": closest_name}
     return result
 
-if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=7892)
 
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=7892)
