ec45d695bf7692133eb768cf1ae9f6d485606bfe
Miles Acquaviva <milesacq@icloud.com>
2024-02-04 00:33:53 -0500

    updated server::

--- star-server/main.py
+++ star-server/main.py
@@ -11,11 +11,12 @@ app = Flask(__name__)
 def calculate_distance():
     longitude = float(request.args.get('longitude'))
     latitude = float(request.args.get('latitude'))
+    planet_name = request.args.get('planet')
 
     ts = load.timescale()
     t = ts.now()
     planets = load("de421.bsp")
-    earth, mars = planets["earth"], planets["mars"]
+    earth, mars = planets["earth"], planets[planet_name]
 
     astrometric = earth.at(t).observe(mars)
 
@@ -26,51 +27,50 @@ def calculate_distance():
     print(alt)
     print(az)
 
-    xyz = astrometric.position.to(u.au)
     altitude = alt.to(u.deg)
 
-    print(xyz)
     print("{0:0.03f}".format(altitude))
 
-    result = f"Azimuth:{az}, Altitude:{altitude}"
+    result = {"Azimuth": float(az.degrees), "Altitude": float(altitude.value)}
 
     return result
 
 @app.route('/closest_planet', methods=['GET'])
 def closest_planet():
-    longitude = float(request.args.get('longitude'))
     latitude = float(request.args.get('latitude'))
+    longitude = float(request.args.get('longitude'))
     direction = float(request.args.get('direction'))
     planets = load("de421.bsp")
     planets_visible = [
         planets["venus"],
         planets["mars"],
-        planets["jupiter BARYCENTER"],
-        planets["saturn BARYCENTER"],
-        planets["uranus BARYCENTER"],
-        planets["neptune BARYCENTER"],
+        planets["sun"],
+        planets["mercury"],
+        planets["moon"],
     ]
     ts = load.timescale()
     t = ts.now()
     
-    earth, mars = planets["earth"], planets["mars"]
+    earth = planets["earth"]
 
-    astrometric = earth.at(t).observe(mars)
-    boston = earth + wgs84.latlon(longitude * N, latitude * W)
+    boston = earth + wgs84.latlon(latitude * N, longitude * W)
 
-    distance = sys.maxint
+    distance = sys.maxsize
     closest_name = None
     for planet in planets_visible:
         astrometric = boston.at(t).observe(planet)
         alt, az, d = astrometric.apparent().altaz()
+        print(direction)
         print(az.degrees)
         if (az.degrees - direction < distance):
-            closest_name = planet
-    
-    result = f"{closest_name}"
+            closest_name = str(planet)
+            closest_name = closest_name.split(" ")
+            closest_name = closest_name[len(closest_name) - 1]
+
+    result = {"name":closest_name}
 
     return result
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=7892)
 
