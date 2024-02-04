ec7550ccb0babab0926800a97318b1e792e3220a
cassiusbeep <cassius_hall@brown.edu>
2024-02-04 09:37:26 -0500

    trying new ard method

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,4 +1,3 @@
-#include <Servo.h>
 #include <Stepper.h>
 
 // defines number of steps in the stepper (x rotator)
@@ -6,62 +5,43 @@
 
 Stepper stepperX(STEPS, 8, 9, 10, 11); // create stepper(step_no, pins)
 Stepper stepperY(STEPS, 4, 5, 6, 7); // create stepper(step_no, pins)
-// Servo servoY; // create servo object for vertical servo
-
-int pos_x = 0; // variable to store servo x's position
-int pos_y; // need to reset servo y's position
 
 void setup() {
   // servoY.attach(); // TODO: find out pins for y servo
 
   // start serial port at 9600 bps and wait for port to open:
-  Serial.begin(9600); // initialise USB listener
-  // Serial1.begin(38400);
+  Serial.begin(9600, 12); // initialise USB listener
   stepperX.setSpeed(60);
   stepperY.setSpeed(60);
 }
 
 void loop() {
-  // if (Serial.available() > 0) {
-  //   Seral.write(1);
-  //   String mvmtString = Serial.readString(); // read input from USB
-  //   Serial.print(mvmtString); // print received string to USB
-  //   // split string into X and Y arguments
-  //   int separator = mvmtString.indexOf(",");
-  //   int xSteps = mvmtString.substring(0, separator).toInt();
-  //   int ySteps = mvmtString.substring(separator + 1, mvmString.length - 1).toInt();
-  //   // send X and Y to steppers
-  //   stepperX.step(xSteps);
-  //   stepperY.step(ySteps);
-  //   Serial.write(1); // confirm orientation complete
-  // }
-
   if (Serial.available() > 0) {
-    char read_byte = Serial.read();
-
-    char* mvmtBytes[12];
+    const byte numChars = 12;
+    char receivedChars[numChars];
+    
     int pos = 0;
+    char c;
 
     while (Serial.available() > 0) { // read the incoming byte:
-      delay(10);  //small delay to allow input buffer to fill
-      char c = Serial.read();  //gets one byte from serial buffer
+      c = Serial.read();  //gets one byte from serial buffer
       
-      if (c == '!' && (pos < 11)) {
-        if (pos > 0) {
-          String mvmtString(*mvmtBytes);
-          int separator = mvmtString.indexOf(",");
-          int xSteps = mvmtString.substring(0,separator).toInt();
-          int ySteps = mvmtString.substring(separator + 1).toInt();
-          stepperX.step(xSteps);
-          stepperY.step(ySteps);
-          mvmtString=""; //clears variable for new input
-          pos = 0;
-        } //breaks out of capture loop to print readstring
+      if (c == '!') {
+        // finish. parse and process the received data
+        receivedChars[pos] = c;
+        String mvmtString = String(receivedChars);
+        int separator = mvmtString.indexOf(",");
+        int xSteps = mvmtString.substring(0,separator).toInt();
+        int ySteps = mvmtString.substring(separator + 1).toInt();
+        stepperX.step(xSteps);
+        stepperY.step(ySteps);
+        mvmtString=""; //clears variable for new input
+        memset(receivedChars, 0, sizeof(receivedChars));
+        pos = 0; 
       } else {
-        mvmtBytes[pos] = c;
+        receivedChars[pos] = c;
         pos++;
       }
-      
     } 
   }
     
