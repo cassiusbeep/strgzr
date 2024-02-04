f0c682f9bfa03e2a9b15b73fdf14fe9e319fb93e
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2024-02-04 03:01:15 -0500

    doesnt work :(

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -6,17 +6,28 @@ import {
   View,
   Platform,
   PermissionsAndroid,
+  NativeEventEmitter,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
-import { UsbSerial } from "react-native-usbserial";
-import { BleManager } from "react-native-ble-plx";
+import BleManager, {
+  BleDisconnectPeripheralEvent,
+  BleManagerDidUpdateValueForCharacteristicEvent,
+  BleScanCallbackType,
+  BleScanMatchMode,
+  BleScanMode,
+  Peripheral,
+} from "react-native-ble-manager";
+
+const SECONDS_TO_SCAN_FOR = 10;
+const SERVICE_UUIDS = ["00001101-0000-1000-8000-00805f9b34fb"];
+const ALLOW_DUPLICATES = true;
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 export default function App() {
   const [location, setLocation] = useState(null);
-
-  const manager = new BleManager();
-  let usbs;
 
   useEffect(() => {
     const watchLocation = async () => {
@@ -65,57 +76,67 @@ export default function App() {
       return false;
     };
 
-    if (Platform.OS === "android") {
-      usbs = new UsbSerial();
-      const getDevice = async () => {
-        const deviceList = await usbs.getDeviceListAsync();
-        const firstDevice = deviceList[0];
-        console.log(firstDevice);
-
-        if (firstDevice) {
-          const usbSerialDevice = await usbs.openDeviceAsync(firstDevice);
-          console.log(usbSerialDevice);
+    const startScan = async () => {
+      await BleManager.scan(
+        SERVICE_UUIDS,
+        SECONDS_TO_SCAN_FOR,
+        ALLOW_DUPLICATES,
+        {
+          matchMode: BleScanMatchMode.Sticky,
+          scanMode: BleScanMode.LowLatency,
+          callbackType: BleScanCallbackType.AllMatches,
         }
-      };
+      );
+    };
 
-      getDevice();
-    }
+    BleManager.start({ showAlert: false })
+      .then(() => console.debug("BleManager started."))
+      .catch((error) =>
+        console.error("BeManager could not be started.", error)
+      );
+
+    const listeners = [
+      bleManagerEmitter.addListener(
+        "BleManagerDiscoverPeripheral",
+        async (peripheral) => {
+          await connectDevice(peripheral);
+        }
+      ),
+      bleManagerEmitter.addListener("BleManagerConnectPeripheral", (device) => {
+        console.log("Connected to device", device.id);
+      }),
+      bleManagerEmitter.addListener(
+        "BleManagerDisconnectPeripheral",
+        (device) => {
+          console.log("Disconnected from device", device.id);
+        }
+      ),
+    ];
 
     requestBluetoothPermission();
     watchLocation();
+    startScan();
+
+    return () => {
+      listeners.forEach((listener) => listener.remove());
+    };
   }, []);
 
-  useEffect(() => {
-    const subscription = manager.onStateChange((state) => {
-      if (state === "PoweredOn") {
-        scanAndConnect();
-        subscription.remove();
-      }
-    }, true);
-    return () => subscription.remove();
-  }, [manager]);
-
-  const scanAndConnect = () => {
-    manager.startDeviceScan(null, null, (error, device) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      if (device.name === "HC-05") {
-        // Stop scanning as it's not necessary if you are scanning for one device.
-        manager.stopDeviceScan();
-
-        device
-          .connect({ autoConnect: true })
-          .then((device) => {
-            console.log("Connected to device", device.id);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }
-    });
+  const connectDevice = async (peripheral: Peripheral) => {
+    try {
+      await BleManager.connect(peripheral.id);
+      console.log("Connected to", peripheral.id);
+      await sleep(900);
+      const peripheralData = await BleManager.retrieveServices(peripheral.id);
+      await BleManager.write(
+        peripheral.id,
+        peripheralData.characteristics[0].service,
+        peripheralData.characteristics[0].characteristic,
+        new TextEncoder().encode("hello")
+      );
+    } catch (error) {
+      console.error("Could not connect to", peripheral.id, error);
+    }
   };
 
   return (
