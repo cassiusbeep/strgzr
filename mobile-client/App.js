1215717369520ea0c63d19ca231fb5d9701eb1d2
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-02-04 11:02:45 -0500

    mobile app (#4)
    * things r broken
    
    * weird gyp ERR!
    
    * usbs?
    
    * add lock files
    
    * workin on server stuff
    
    * axios request bug
    
    * fixed axios thing :) now just need to fix the planet name passing error
    
    * axios req not working due to undefined planet name in App.js moveArm()
    
    * cleanup files
    
    * starting on server comms
    
    * styling and cleaning up arduino file
    
    * added bytewise reading to arduino
    
    * arduino retry
    
    * fix
    
    * compiles this time
    
    * trying new ard method
    
    * fixed planet name issue
    
    * last min ui stuff
    
    ---------
    
    Co-authored-by: cassiusbeep <cassius_hall@brown.edu>

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View, Platform, FlatList } from "react-native";
+import { Pressable, Button, StyleSheet, Text, View, Platform, FlatList, SafeAreaView, Modal } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
 import { UsbSerial } from "react-native-usbserial";
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import axios from 'axios';
 
 export default function App() {
-  
   const [usbConnected, setUsbConnected] = useState(false);
   const Stack = createNativeStackNavigator();
   const [location, setLocation] = useState(null);
@@ -17,16 +16,20 @@ export default function App() {
   const [lat, setLat] = useState(null);
   const [azimuth, setAzimuth] = useState(null);
   const [altitude, setAltitude] = useState(null);
+  const [lastDirs, setLastDirs] = useState(null);
+  const [currentPlanet, setCurrentPlanet] = useState("");
+  const [invisErrorOpen, setInvisErrorOpen] = useState(false);
 
-  const getDirections = async (long, lat, planet) => {
-    console.log(long);
-    console.log(lat);
-    console.log(planet);
-    
-    axios.get('http://server.milesacq.com:7892/calculate_distance', 
-      {longitude: long,
-      latitude: lat,
-      planet: planet}
+  const getDirections = (long, lat, planet) => {
+    axios.get('http://server.milesacq.com:7892/calculate_distance',
+      {
+        params: {
+          longitude: long,
+          latitude: lat,
+          planet: planet,
+          heading: location
+        }
+      }
     ).then((response) => {
       setAzimuth(response.data.Azimuth);
       setAltitude(response.data.Altitude);
@@ -35,18 +38,28 @@ export default function App() {
     })
   }
 
-  const getPlanets = (long, lat, direction) => {
-    planetRouter.get(
-      "http://server.milesacq.com:7892/closest_planet", {
+  const sendDirections = (dirxns) => {
+    axios.post('http://server.milesacq.com:7892/move_servos',
+      {
         params: {
-          longitude,
-          latitude,
-          direction
+          dirString: dirxns
         }
       }
     )
   }
-  
+
+  // const getPlanets = (long, lat, direction) => {
+  //   planetRouter.get(
+  //     "http://server.milesacq.com:7892/closest_planet", {
+  //     params: {
+  //       longitude,
+  //       latitude,
+  //       direction
+  //     }
+  //   }
+  //   )
+  // }
+
   let usbs;
 
   useEffect(() => {
@@ -85,26 +98,7 @@ export default function App() {
     }
   }, []);
 
-  useEffect(() => {
-
-  })
-
-  const BODIES = [
-    {
-      id: '1',
-      title: 'Venus',
-    },
-    {
-      id: '2',
-      title: 'Jupiter',
-    },
-    {
-      id: '3',
-      title: 'Mars',
-    },
-  ];
-
-  function HomeScreen({ navigation }) { 
+  function HomeScreen({ navigation }) {
     useEffect(() => {
       if (usbConnected && usbs) {
         navigation.navigate('Dock')
@@ -115,68 +109,128 @@ export default function App() {
       <View style={styles.container}>
         <Text>Compass heading: {Math.round(location?.trueHeading)}°</Text>
         <Text>Place phone in telescope dock!</Text>
-        <Button onPress={() => {
-          navigation.navigate('Dock')}} title={"Search the Stars"}></Button>
+        <Pressable onPress={() => {
+          navigation.navigate('Dock')
+        }}
+        style={styles.button}><Text style={styles.buttonText}>Search the Stars</Text></Pressable>
       </View>
     );
   }
 
-  function moveArm({navigation, planetName}) {
-    console.log(planetName);
-    console.log('move arm');
-    getDirections(long, lat, planetName).then(() => {
-      // work out number of 5.625° steps to take to point there
-      if (altitude < 0 || altitude > 90) {
-        console.log(planetName + "not visible");
-        // dont move the arm!
-      } else {
-        // TODO: check if this is appropriate calibration to turn the middle wheel
-        const xSteps = Math.round(((azimuth - location?.trueHeading)/5.625)*0.5);
-        const ySteps = Math.round(altitude/5.625);
-        const dirString = xSteps + "," + ySteps;
-        console.log("direction string: ")
-        console.log(dirString);
-        // TODO: send new azimuth and altitude to the arduino
-        navigation.navigate('TelescopeScreen');
-      }
-    });
+  function moveArm(navigation, planetName) {
+    setCurrentPlanet(planetName);
+    getDirections(long, lat, planetName);
+    // work out number of 5.625° steps to take to point there
+    // if (altitude < 0 || altitude > 90) {
+    //   console.log(planetName + " not visible"); // dont move the arm!
+    // } else {
+      // TODO: check if this is appropriate calibration to turn the middle wheel
+      const xSteps = Math.round(((azimuth - location?.trueHeading) / 5.625) * 0.5);
+      const ySteps = Math.round(altitude / 5.625);
+      const dirString = xSteps + "," + ySteps + "!";
+      // send arduino direction string to the server to be passed into usb serial port
+      sendDirections(dirString);
+      setLastDirs(dirString);
+      navigation.push('Telescope');
+    // }
   }
 
-  function DockScreen({navigation}) { 
+  BODIES = [
+    {
+      id: '10',
+      title: 'sun',
+    },
+    {
+      id: '199',
+      title: 'mercury',
+    },
+    {
+      id: '399',
+      title: 'earth',
+    },
+    {
+      id: '299',
+      title: 'venus',
+    },
+    {
+      id: '301',
+      title: 'moon',
+    },
+    {
+      id: '499',
+      title: 'mars',
+    },
+  ]
+
+  const Item = ({navigation, title}) => (
+    <View style={styles.item}>
+      <Pressable onPress={() => moveArm(navigation, title)} style={styles.button}><Text style={styles.buttonText}>{title}</Text></Pressable>
+    </View>
+  );
+
+  function DockScreen({ navigation }) {
     return (
       <View style={styles.container}>
-        <Text>Compass heading: {Math.round(location?.trueHeading)}°</Text>
-        <Button title={"sun"} onPress={(event) => moveArm(navigation, "mars")}></Button>
-        <Button onPress={() => {navigation.goBack()}} title={"back"}></Button>
-        
-        {/* <SafeAreaView style={styles.container}>
+        <Text style={{marginTop: '160px'}}>Compass heading: {Math.round(location?.trueHeading)}°</Text>
+
+        <Pressable 
+          onPress={() => { navigation.goBack() }} 
+          title={"back"} 
+          style={styles.button}>
+          <Text style={styles.buttonText}>back</Text>
+        </Pressable>
+
+        {/* <Modal
+        animationType='slide'
+        transparent={false}
+        visible={invisErrorOpen}
+        onRequestClose={() => {
+          setInvisErrorOpen(false);
+        }}> */}
+          {/* <View style={styles.modalView}>
+            <Text style={styles.modalText}>Planet is not visible!</Text>
+            <Pressable onPress={() => setInvisErrorOpen(!invisErrorOpen)} style={styles.button}>
+              <Text style={styles.buttonText}>close</Text>
+            </Pressable>
+          </View> */}
+        {/* </Modal> */}
+        <SafeAreaView style={styles.container}>
           <FlatList
             data={BODIES}
-            renderItem={({id, title}) => 
-                {return (
-                  <View style={styles.container}>
-                    <Button onPress={(title) => moveArm(navigation, title)} title={title}></Button>
-                  </View>)
-                }}
+            renderItem={({item}) => <Item navigation={navigation} title={item.title}/>}
             keyExtractor={item => item.id}
-          />
-        </SafeAreaView> */}
+            />
+        </SafeAreaView>
       </View>
     );
   }
 
-  function TelescopeScreen({planet, navigation}) { 
+  function TelescopeScreen({ navigation }) {
+    // find the opposite of the directions it took to orient here for the exit
+    lastXStep = lastDirs.split(",")[0];
+    lastYStep = lastDirs.split(",")[1];
+    const resetDirs = (-1 * lastXStep) + "," + (-1 * lastYStep);
     return (
       <View style={styles.container}>
         <Text>Compass heading: {Math.round(location?.trueHeading)}°</Text>
-
+        <Text>NOW VIEWING: {typeof currentPlanet == String ? currentPlanet : ""}</Text>
+        <Pressable onPress={() => {
+          // sendDirections(resetDirs);
+          setCurrentPlanet(null);
+          navigation.goBack();}}
+          style={styles.button}><Text style={styles.buttonText}>back</Text></Pressable>
       </View>
     );
   }
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator 
+        initialRouteName="Home"
+        screenOptions={{
+          headerTransparent: true,
+          headerBlurEffect: 'systemChromeMaterialLight'
+        }}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -200,9 +254,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    display: 'flex',
+    backgroundColor: "#592A88",
     alignItems: "center",
     justifyContent: "center",
-    width: '100%'
+    width: '100%',
+    color: '#fff',
+    marginTop: '125px',
+  },
+  item: {
+    backgroundColor: '#592A88',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    color: '#fff',
+  },
+  title: {
+    fontSize: 32,
+    color: '#fff',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: '#461C71',
+    color: '#fff',
   },
 });
